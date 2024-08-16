10784a1a0cfc71d0beac1ad8556ea9986ea32e69
vincent <gvincent@163.com>
2024-08-16 22:30:23 +0800

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Vincent on 2024/8/16.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherDemoTests/WeatherDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoTests.swift
+//  WeatherDemoTests
+//
+//  Created by Vincent on 2024/8/16.
+//
+
+import Testing
+@testable import WeatherDemo
+
+struct WeatherDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
